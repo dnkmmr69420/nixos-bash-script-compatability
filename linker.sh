ba0a2cf24eacb79e1fc7442280d6d2495f3878df
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-22 09:21:04 -0500

    Update linker.sh

--- linker.sh
+++ linker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 sudo echo "Creating symlinks..."
-sudo ln -s /run/current-system/sw/bin/bash /bin/bash
-sudo ln -s /run/current-system/sw/bin/bash /usr/bin/bash
-sudo ln -s /run/current-system/sw/bin/sh /usr/bin/sh
-sudo ln -s /run/current-system/sw/bin/env /bin/env
+sudo rm -rf /bin
+sudo ln -sf /usr/bin /bin
+sudo ln -sf /run/current-system/sw/bin/bash /usr/bin/bash
+sudo ln -sf /run/current-system/sw/bin/sh /usr/bin/sh
+sudo ln -sf /run/current-system/sw/bin/sudo /usr/bin/sudo
 sudo echo "Finnished the symlinking process."
