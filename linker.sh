3e2fab37d8b5873c9ef1a04025474ac943b229be
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-05-26 08:48:33 -0500

    Update linker.sh

--- linker.sh
+++ linker.sh
@@ -4,5 +4,5 @@ sudo rm -rf /bin
 sudo ln -sf /usr/bin /bin
 sudo ln -sf /run/current-system/sw/bin/bash /usr/bin/bash
 sudo ln -sf /run/current-system/sw/bin/sh /usr/bin/sh
-sudo ln -sf /run/current-system/sw/bin/sudo /usr/bin/sudo
+sudo ln -sf /run/wrappers/bin/sudo /usr/bin/sudo
 sudo echo "Finnished the symlinking process."
